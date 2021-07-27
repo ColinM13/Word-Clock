8ee9738625bea88fd14f1e1c4cc4e785cc31940c
Colin Miller <cmiller@Colins-MacBook-Air.local>
2021-07-27 15:05:57 -0400

    Added CLI args functionality for using default clock time or inputting custom 24 hour time.

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,22 @@ import app.WordClock;
 public class Main {
 
     public static void main(String[] args) {
-        WordClock wc = new WordClock(25, 0);
-        wc.printWordTime();
+        if(args.length == 0) {
+            WordClock wc = new WordClock();
+            wc.printWordTime();
+        }
+        if(args.length == 2) {
+            int hour;
+            int minute;
+            try {
+                hour = Integer.parseInt(args[0]);
+                minute = Integer.parseInt(args[1]);
+                WordClock wc = new WordClock(hour, minute);
+                wc.printWordTime();
+            } catch (NumberFormatException e) {
+                System.out.println("Arguments must be typed as valid 24 hour clock numbers");
+            }
+        }
     }
+
 }
